42bf5334b4a30e8d67b33f4b68e898ad4c21bdd1
arthurbatista04 <arthurbatista04@outlook.com>
2020-11-16 15:49:32 -0300

    com printhtml

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+// http://www.printhtml.com/ é necessário para funcionar
 const express = require("express");
 const bodyParser = require("body-parser");
-const nodeHtmlToImage = require("node-html-to-image");
 const app = express();
 const port = 3000;
 
@@ -16,21 +16,13 @@ app.post("/", (req, res) => {
     aplicador: "Joao da silva",
     dataAplicacao: "hoje",
   };
-  nodeHtmlToImage({
-    output: "./image.png",
-    html: `<html>  <style>body {width: 300px;height: 200px;}</style><body><div style="width:300px;height:200px;background-color:#f5f5f5;display:flex;flex-direction:column;justify-content:space-around; align-items:center;">
-
-	<p  style="font-size:18px; margin: 0px; color:#424242"><b>${nomeComecial}</b></p>
-	
-	<p style="font-size:15px; font-weight:bold;color:#424242; margin: 0px">${nomeGenerico}</p>
-	
-	<p style="font-size:18px; font-weight:bold; margin:0px; color: color:#424242">Lote: ${lote}</p> 
-	
-	<p style="font-size:18px; font-weight:bold; color:color:#424242; margin: 0px">${dataAplicacao}</p>
-	
-	<p style="font-size:18px; font-weight: bold; margin: 0px; color:color:#424242">Enf. ${aplicador}</p>
-	</div></body></html>`,
-  }).then(() => res.sendStatus(200));
+  var exec = require("child_process").exec;
+  exec(
+    `printhtml.exe html=<html>  <style>body {width: 100px;height: 120px;}</style><body><div style="width:100px;height:120px;background-color:#f5f5f5;display:flex;flex-direction:column;justify-content:space-around; align-items:center;"><p  style="font-size:18px; margin: 0px; color:#424242"><b>${nomeComecial}</b></p><p style="font-size:15px; font-weight:bold;color:#424242; margin: 0px">${nomeGenerico}</p><p style="font-size:18px; font-weight:bold; margin:0px; color: color:#424242">Lote: ${lote}</p> <p style="font-size:18px; font-weight:bold; color:color:#424242; margin: 0px">${dataAplicacao}</p><p style="font-size:18px; font-weight: bold; margin: 0px; color:color:#424242">Enf. ${aplicador}</p></div></body></html>`,
+    function (err, data) {
+      console.log(data.toString());
+    }
+  );
 });
 
 app.listen(port, () => {
