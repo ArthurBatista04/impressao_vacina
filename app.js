154a4bbf3b8361614e9ccf9d792490c1cc0157bc
arthurbatista04 <arthurbatista04@outlook.com>
2020-11-17 16:23:44 -0300

    other image gen

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // IrfanView é necessário para funcionar
 const express = require("express");
 const bodyParser = require("body-parser");
-const nodeHtmlToImage = require("node-html-to-image");
+var fs = require("fs");
+var text2png = require("text2png");
 const app = express();
 const port = 3007;
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,44 +16,26 @@ const imprimir = async () => {
     await exec(
       '"C:\\Program Files\\IrfanView\\i_view64.exe" .\\image.png /print'
     );
-    console.log('tryyyyy')
+    console.log("tryyyyy");
     return 200;
   } catch (error) {
-    console.log('catchhh')
+    console.log("catchhh");
     return 500;
   }
 };
 
-app.post("/", (req, res) => {
-  const { 
-    nomeComercial, 
-    nomeGenerico, 
+app.post("/", async (req, res) => {
+  const {
+    nomeComercial,
+    nomeGenerico,
     lote,
     aplicador,
-    dataAplicacao
+    dataAplicacao,
   } = req.body;
-  
-  const width = '70px'
-  const height = '60px'
-  const fontSize = '10px'
 
-  nodeHtmlToImage({
-    output: "./image.png",
-    html: `<html>  <style>body {width: ${width};height: ${height};}</style><body><div style="width:${width};height:${height};background-color:#f5f5f5;display:flex;flex-direction:column;justify-content:space-around; align-items:center;">
-	<p  style="font-size:${fontSize}; margin: 0px; color:#424242"><b>${nomeComercial}</b></p>
-	
-	<p style="font-size:${fontSize}; font-weight:bold;color:#424242; margin: 0px">${nomeGenerico}</p>
-	
-	<p style="font-size:${fontSize}; font-weight:bold; margin:0px; color: color:#424242">Lote: ${lote}</p> 
-	
-	<p style="font-size:${fontSize}; font-weight:bold; color:color:#424242; margin: 0px">${dataAplicacao}</p>
-	
-	<p style="font-size:${fontSize}; font-weight: bold; margin: 0px; color:color:#424242">Enf. ${aplicador}</p>
-	</div></body></html>`,
-  }).then(async () => {
-	  const status = await imprimir();
-    res.sendStatus(200)
-  });
+  fs.writeFileSync("out.png", text2png(nomeGenerico));
+  const status = await imprimir();
+  res.sendStatus(status);
 });
 
 app.listen(port, () => {
